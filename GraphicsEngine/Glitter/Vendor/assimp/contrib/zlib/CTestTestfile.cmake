53aad29c8c5ab941497e5f0a428e611ed48c31c7
Daniel Andersen <gadren@gmail.com>
2015-08-18 21:13:38 -0400

    init with Glitter

---
+++ GraphicsEngine/Glitter/Vendor/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Files/Projects/Glitter/Glitter/Vendor/assimp/contrib/zlib
+# Build directory: D:/Files/Projects/Glitter/Build/Glitter/Vendor/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
